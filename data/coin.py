660a9dec48d216bef8c1ceb0644256a0262f5625
vaZZZy1 <rigbitvchannel@yandex.ru>
2023-01-02 18:22:03 +0300

    Revert "Refactor arbitation project: отсортировал программы по смыслу, удалил ненужные файлы"
    This reverts commit be3d3b5dcfaf2e49c87a786037b8da85ed80e12c.

---
+++ data/coin.py
@@ -0,0 +1,6 @@
+class Coin:
+    def __init__(self, name, id):
+        self.id = id
+        self.name = name
+        self.course = 0
+        self.capitalization = 0
--- data/okx_parser.py
+++ data/okx_parser.py
@@ -6,24 +6,29 @@ from time import sleep
 
 def get_name(el):
     name = el.find_element(By.CLASS_NAME, "advertiser-name")
+    #print(name.text)
     return name.text
 
 
 def get_col_orders(el):
     col_el = el.find_element(By.CLASS_NAME, "advertiser-info")
     col = col_el.find_element(By.CSS_SELECTOR, "span")
+    #print(int(col.text.split()[0]))
     return int(col.text.split()[0])
 
 
 def get_complete_percent(el):
     percent = el.find_element(By.CLASS_NAME, "execute-rate")
     percent_str = percent.text
+    #print(float(percent_str.split("%")[0]))
     return float(percent_str.split("%")[0])
 
 
 def get_price(el):
     price = el.find_element(By.CLASS_NAME, "price-amount")
     txt = price.text
+    #print(1, txt)
+    #print(float("".join(txt.split(","))))
     return float("".join(txt.split(",")))
 
 
@@ -34,17 +39,20 @@ def get_limit(el):
         lim = el.split()[0]
         lim = "".join(lim.split(","))
         mas_lim += [float(lim)]
+    #print(mas_lim)
     return mas_lim
 
 
 def get_available(el):
     price = el.find_elements(By.CLASS_NAME, "ql-value")
     txt = price[0].text.split()[0]
+    #print("".join(txt.split(",")))
     return float("".join(txt.split(",")))
 
 
 def get_glass_position(driver):
     pos = []
+    #table = driver.find_element(By.CLASS_NAME, "trade-list__content")
     pos_element = driver.find_elements(By.XPATH, "//table/tbody/tr")
     kol = min(7, len(pos_element))
     for element in pos_element[:kol]:
@@ -105,6 +113,7 @@ def parse(link, limit):
         sleep(1)
         try:
             element = driver.find_elements(By.XPATH, "//table/tbody/tr")
+            #print(len(element))
             if len(element) > 1:
                 break
         except Exception:
@@ -117,3 +126,5 @@ def parse(link, limit):
 
 if __name__ == "__main__":
     print(parse("https://www.okx.cab/ru/p2p-markets/rub/sell-eth", 10000))
+
+

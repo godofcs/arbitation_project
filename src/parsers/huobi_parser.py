d299640f096e21f18519f5d0b7057c703692cc23
vaZZZy1 <rigbitvchannel@yandex.ru>
2023-01-02 19:56:24 +0300

    Фикс кривого мерджа

---
+++ src/parsers/huobi_parser.py
@@ -0,0 +1,157 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common import keys
+from time import sleep
+
+
+def get_name(el):
+    name = el.find_element(By.CLASS_NAME, "font14")
+    #print(name.text)
+    return name.text
+
+
+def get_col_orders(el):
+    col = el.find_element(By.CLASS_NAME, "grey-label-half")
+    #print(int(col.text.split(":")[1].split("|")[0]))
+    return int(col.text.split(":")[1].split("|")[0])
+
+
+def get_complete_percent(el):
+    percent = el.find_element(By.CLASS_NAME, "grey-label-half")
+    percent_str = percent.text.split(":")[1].split("|")[0]
+    #print(float(percent_str.split("%")[0]))
+    return float(percent_str.split("%")[0])
+
+
+def get_price(el):
+    price = el.find_element(By.CLASS_NAME, "price")
+    price = price.find_element(By.CSS_SELECTOR, "div")
+    txt = price.text.split()[0]
+    #print(1, txt)
+    #print(float("".join(txt.split(","))))
+    return float("".join(txt.split(",")))
+
+
+def get_limit(el):
+    pre_limit = el.find_element(By.CLASS_NAME, "limit")
+    limit = pre_limit.find_elements(By.CSS_SELECTOR, "span")
+    lim1 = limit[0].text
+    lim2 = limit[1].text
+    lim2 = "".join(lim2.split(","))
+    lim2 = lim2.split("-")[1]
+    lim2_txt = lim2.split(".")[0] + "."
+    for el in lim2.split(".")[1]:
+        if el.isdigit():
+            lim2_txt = lim2_txt + el
+        else:
+            break
+    mas_lim = [float("".join(lim1.split(","))), float(lim2_txt)]
+    #print(mas_lim)
+    return mas_lim
+
+
+def get_available(el):
+    price = el.find_element(By.CLASS_NAME, "stock")
+    pre_txt = price.text
+    txt = pre_txt.split(".")[0] + "."
+    for el in pre_txt.split(".")[1]:
+        if el.isdigit():
+            txt = txt + el
+        else:
+            break
+    #print("".join(txt.split(",")))
+    return float("".join(txt.split(",")))
+
+
+def get_glass_position(driver):
+    pos = []
+    #table = driver.find_element(By.CLASS_NAME, "trade-list__content")
+    pos_element = driver.find_elements(By.CLASS_NAME, "otc-trade-list")
+    kol = min(7, len(pos_element))
+    for element in pos_element[:kol]:
+        #print(element)
+        try:
+            data = {
+                "name": get_name(element),
+                "col_orders": get_col_orders(element),
+                "complete_percent": get_complete_percent(element),
+                "price": get_price(element),
+                "limit": get_limit(element),
+                "available": get_available(element)
+            }
+            pos += [data]
+        except Exception:
+            print(Exception)
+            pass
+    return pos
+
+
+def parse(link, limit, cur_payment):
+    driver = Chrome(executable_path="./chromedriver.exe")
+    driver.get(link)
+    # Это на всякий случай
+    kol = 0
+    while kol < 30:
+        sleep(1)
+        try:
+            button = driver.find_element(By.CLASS_NAME, "video-close")
+            if button:
+                button.click()
+                break
+        except Exception:
+            pass
+        kol += 1
+    kol = 0
+    while kol < 30:
+        sleep(1)
+        try:
+            payment = driver.find_element(By.CLASS_NAME, "search-amount-container")
+            if payment:
+                input_place = payment.find_element(By.CLASS_NAME, "ivu-input")
+                input_place.send_keys(f"{limit}")
+                button = payment.find_element(By.CLASS_NAME, "submit-in")
+                button.click()
+                break
+        except Exception:
+            pass
+        kol += 1
+    kol = 0
+    while kol < 30:
+        sleep(1)
+        try:
+            pre = driver.find_elements(By.CLASS_NAME, "pay-search-container")[2]
+            if pre:
+                pre.click()
+                pre_input = driver.find_element(By.CLASS_NAME, "pay-method-haveHistory")
+                input = pre_input.find_element(By.CLASS_NAME, "ivu-input-default")
+                input.send_keys(f"{cur_payment}")
+                sleep(0.5)
+                but = pre_input.find_element(By.CLASS_NAME, 'cursor')
+                but.click()
+                sleep(0.5)
+                but = pre_input.find_element(By.CLASS_NAME, 'multiSelect-payments-confirm')
+                but.click()
+                break
+        except Exception:
+            pass
+        kol += 1
+    kol = 0
+    while kol < 30:
+        sleep(1)
+        try:
+            element = driver.find_elements(By.CLASS_NAME, "otc-trade-list")
+            #print(len(element))
+            if len(element) > 1:
+                break
+        except Exception:
+            pass
+        kol += 1
+    pos = get_glass_position(driver)
+    driver.close()
+    return pos
+
+
+#if __name__ == "__main__":
+#    print(parse("https://www.huobi.com/ru-ru/fiat-crypto/trade/buy-eth-rub/", 10000, "Тинькофф"))
+
+
---
+++ src/parsers/parse_argument.py
@@ -0,0 +1,146 @@
+from data.binance_parser import parse as binance_parse
+from data.bybit_parser import parse as bybit_parse
+from data.analyse import analyse_glass
+from data.huobi_parser import parse as huobi_parse
+import datetime
+from data import db_session
+from data.offers import Offer
+from pyvirtualdisplay import Display
+
+db_session.global_init("C:/Users/4739409/PycharmProjects/arbitation_project/bd/base.sqlite")
+
+all_crypto = {"USDT": {"binance": "USDT", "bybit": "USDT", "huobi": "usdt"},
+              "BTC": {"binance": "BTC", "bybit": "BTC", "huobi": "btc"},
+              "BUSD": {"binance": "BUSD", "bybit": "-", "huobi": "-"},
+              "BNB": {"binance": "BNB", "bybit": "-", "huobi": "-"},
+              "ETH": {"binance": "ETH", "bybit": "ETH", "huobi": "eth"},
+              "SHIB": {"binance": "SHIB", "bybit": "-", "huobi": "-"}}
+all_payment = {"Tinkoff": {"binance": "TinkoffNew", "bybit": "75", "huobi": "Тинькофф"},
+               "Sberbank": {"binance": "RosBankNew", "bybit": "185", "huobi": "Сбербанк"},
+               "Raiffeisenbank": {"binance": "RaiffeisenBank", "bybit": "64", "huobi": "Райффайзенбанк"},
+               }  # "QIWI", "YandexMoneyNew"
+all_fiat = {"RUB": {"binance": "RUB", "bybit": "RUB", "huobi": "rub"}}
+mas_sell_buy = ["sell", "buy"]
+x = 1
+all_market = {"binance":
+    {
+        "buy": ["https://p2p.binance.com/ru/trade/", "key=payment", "/", "key=crypto", "?fiat=", "key=fiat"],
+        "sell": ["https://p2p.binance.com/ru/trade/sell/", "key=crypto", "?fiat=", "key=fiat", "&payment=",
+                 "key=payment"]
+    },
+    "bybit":
+        {
+            "buy": ["https://www.bybit.com/fiat/trade/otc/?actionType=1&token=", "key=crypto", "&fiat=", "key=fiat",
+                    "&paymentMethod=", "key=payment"],
+            "sell": ["https://www.bybit.com/fiat/trade/otc/?actionType=0&token=", "key=crypto", "&fiat=", "key=fiat",
+                     "&paymentMethod=", "key=payment"]
+        },
+    "huobi":
+        {
+            "buy": ["https://www.huobi.com/ru-ru/fiat-crypto/trade/buy-", "key=crypto", "-", "key=fiat", "/"],
+            "sell": ["https://www.huobi.com/ru-ru/fiat-crypto/trade/sell-", "key=crypto", "-", "key=fiat", "/"]
+        }
+}
+
+limits = {1: 1000, 2: 5000, 3: 10000, 4: 25000, 5: 50000, 6: 100000, 7: 500000, 8: 1000000}
+
+
+def get_limit(limit_id):
+    return limits[limit_id]
+
+
+def make_mas_links(cur_fiat, cur_market, cur_crypto, cur_payment):
+    mas_links = []
+    for sell_buy in mas_sell_buy:
+        for payment in cur_payment:
+            for fiat in cur_fiat:
+                for crypto in cur_crypto:
+                    for market in cur_market:  # Плохо, переделать
+                        link = ""
+                        flag = 0
+                        for element in all_market[market][sell_buy]:
+                            if "key=" in element:
+                                key = element.split("=")[1]
+                                if key == "payment":
+                                    if all_payment[payment][market] == "-":
+                                        flag = 1
+                                        break
+                                    link = link + all_payment[payment][market]
+                                elif key == "crypto":
+                                    if all_crypto[crypto][market] == "-":
+                                        flag = 1
+                                        break
+                                    link = link + all_crypto[crypto][market]
+                                elif key == "fiat":
+                                    if all_fiat[fiat][market] == "-":
+                                        flag = 1
+                                        break
+                                    link = link + all_fiat[fiat][market]
+                            else:
+                                link = link + element
+                        if flag:
+                            break
+                        if sell_buy == "sell":
+                            mas_links.append([link, [market, crypto, fiat, payment, sell_buy]])
+                        else:
+                            mas_links.append([link, [market, fiat, crypto, payment, sell_buy]])
+    print(len(mas_links))
+    for i in mas_links:
+        print(i)
+    return mas_links
+
+
+def add_to_database(new_offer, limit_id, taker_commission, maker_commission):
+    sessions = db_session.create_session()
+    offer = sessions.query(Offer).filter(Offer.market == new_offer[1],
+                                         Offer.init_coin == new_offer[2],
+                                         Offer.receive_coin == new_offer[3],
+                                         Offer.id_limit == limit_id).first()
+    if offer is None:
+        offer = Offer()
+        offer.market = new_offer[1]
+        offer.init_coin = new_offer[2]
+        offer.receive_coin = new_offer[3]
+        offer.id_limit = limit_id
+    offer.price = new_offer[0]
+    offer.las_time_update = new_offer[-1]
+    offer.taker_commission = taker_commission
+    offer.maker_commission = maker_commission
+    sessions.merge(offer)
+    sessions.commit()
+    sessions.close()
+
+
+def checking_the_relevance_of_information():
+
+    pass
+
+
+def parse_argument(limit_id, fiat_mas, market_mas, crypto_mas, payment_mas):
+    limit = get_limit(limit_id)
+    mas_links = make_mas_links(fiat_mas, market_mas, crypto_mas, payment_mas)
+
+    for link in mas_links[:4]:
+        if "binance" in link[0]:
+            glass = binance_parse(link[0], limit)
+            new_offer = [analyse_glass(glass)] + link[1] + [datetime.datetime.now()]
+            print("binance", new_offer)
+            add_to_database(new_offer, limit_id, 0.0, 0.1)
+        elif "bybit" in link[0]:
+            glass = bybit_parse(link[0], limit)
+            new_offer = [analyse_glass(glass)] + link[1] + [datetime.datetime.now()]
+            print("bybit", new_offer)
+            add_to_database(new_offer, limit_id, 0.0, 0.0)
+        elif "huobi" in link[0]:
+            glass = huobi_parse(link[0], limit, all_payment[link[1][3]]["huobi"])
+            print(link[1][3])
+            new_offer = [analyse_glass(glass)] + link[1] + [datetime.datetime.now()]
+            print("huobi", new_offer)
+            add_to_database(new_offer, limit_id, 0.0, 0.0)
+    sessions = db_session.create_session()
+    offers = sessions.query(Offer)
+    ans = []
+    for offer in offers:
+        ans += [[offer.market, offer.init_coin, offer.receive_coin, offer.id_limit, offer.price, offer.maker_commission,
+              offer.taker_commission, offer.las_time_update]]
+    return ans

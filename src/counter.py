093f45d2dea545f5f249574b419f7cc759a82953
vaZZZy1 <rigbitvchannel@yandex.ru>
2023-01-04 06:09:24 +0300

    Изменил логику счетчика: обновил создание графа и возврат ответа, поправил вспомогательные функции

--- src/counter.py
+++ src/counter.py
@@ -1,3 +1,5 @@
+from src.db_requests.offers import Offer
+
 N = 201
 BIT_TO_RUB = 2023536.96
 INF = 100000000
@@ -9,75 +11,65 @@ _market = {"binance": 1, "bybit": 2, "huobi": 3}
 _deM = {1: "binance", 2: "bybit", 3: "huobi"}
 
 
-def PosByCoin(coin):
-    return _crypto[coin.recive_coin] * 10 + _market[coin.market]
+def PosByCoin(coin: Offer, type_of_coin: str):
+    coin_name = coin.recive_coin if type_of_coin == "receive" else coin.init_coin
+    if coin_name.upper() in _fiat:
+        return N - 1
+    return _crypto[coin_name] * 10 + _market[coin.market]
 
 
-def exchange(coin):
+def Exchange(coin):
     if coin.recive_name.upper() in _fiat:
         return 1
     return coin.price * BIT_TO_RUB
 
 
-def commission(coin):
+def Commission(coin):
     # TODO
     return 0
 
 
 def PriceByCoin(coin):
-    return max(coin.price * (1 - coin.taker_commission), coin.price * (1 - coin.maker_commission)) * exchange \
-        (coin) - commission(coin)
+    return max(coin.price * ((100 - coin.taker_commission) / 100),
+               coin.price * ((100 - coin.maker_commission) / 100)) * Exchange \
+               (coin) - Commission(coin)
 
 
 def Counter(data: list):
-    fiat = []
-    crypto = []
-    for coins in data:
-        if coins.receive_name.upper() in _fiat:
-            fiat.append(coins)
-        else:
-            crypto.append(coins)
+    """
+    Поиск релазиован с помощью алгоритма поиска наибольшего пути
+    в ациклическом графе. Для этого нам нужно транспонировать исходный граф,
+    а потом, с помощью методом динамического программирования, найти наиболее
+    большой путь. Мы не будем явно транспонировать граф, мы изначально его зададим
+    в транспонированном виде.
+    """
     gr = [[] for i in range(N)]
-    for toCrypto in crypto:
-        pos_of_node = PosByCoin(toCrypto)
-        pos_of_root = 0
-        if not toCrypto.init_name.upper() in _fiat:
-            pos_of_root = _crypto[toCrypto.recive_coin] * 10 + _market[toCrypto.market]
-        is_maker = toCrypto.maker_commision != 1
-        gr[pos_of_root].append([pos_of_node, PriceByCoin(toCrypto), is_maker])
-    for toFiat in fiat:
-        pos_of_node = PosByCoin(toFiat)
-        is_maker = toFiat.taker_commision != 1  # было maker_comission
-        gr[pos_of_node].append([N - 1, PriceByCoin(toFiat), is_maker])
-    d = [INF for i in range(N)]
-    p = [-1 for i in range(N)]
-    p_m = [0 for i in range(N)]
-    p[0] = -1
-    d[0] = 0
+    for offer in data:
+        if offer.receive_coin.upper() in _fiat:
+            gr[0].append(offer)
+        elif offer.receive_coin in _crypto.keys():
+            gr[PosByCoin(offer, "init")].append(offer)
+        # TODO Вот тут можно прикрутить лог, если пришла непонятная моментка
+    prev = [data[0] for i in range(N)]
+    prev[0] = -1
+    dp = [-INF for i in range(N)]
+    dp[0] = 0
 
-    def dfs(v: int, pred=-1):
+    def dfs(v: int):
         for u in gr[v]:
-            d[u[0]] = max(d[u[0]], d[v] + d[u[1]])
-            if d[u[0]] < d[v] + u[1]:
-                p[u[0]] = v
-                p_m[u[0]] = d[u[2]]
-            dfs(d[u[0]], v)
+            pos = PosByCoin(u, "init")
+            if dp[v] + u.price > dp[pos]:
+                dp[pos] = dp[v] + u.price
+                prev[pos] = u
 
     dfs(0)
-    pos = N
-    ans = ["RUB"]
-    while p[pos] != -1:
-        pos = p[pos]
-        ans.append("->")
-        ans.append(_deC[pos/10])
-        ans.append(_deM[pos%10])
-        if p_m[pos]:
-            ans.append("MAKER")
-        else:
-            ans.append("TAKER")
-        ans.append("|")
-    ans.append("RUB")
-    # return ans
-    for i in ans:
-        print(i, end=" ")
-    return
+    ans = str()
+    pos = N - 1 # или ноль?
+    while pos != -1:
+        cur_offer = prev[pos]
+        # TODO Добавить справку о обозначениях в Хелп
+        ans += "Sell" if cur_offer.sell_buy else "Buy"  # если у тебя тру == бай, то поменяй сам пж
+        ans += "Taker" if cur_offer.maker_commission == 100 else "Maker"
+        ans += cur_offer.market + cur_offer.init_coin + cur_offer.recieve_coin + " -> "
+        pos = PosByCoin(cur_offer, "receive") # тут еще подумать надо
+    return ans

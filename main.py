bb97b66344303e7806a131cfc52c2d667eff2a78
godofcs <chesnov.ilya@bk.ru>
2023-01-05 20:59:25 +0300

    Доделал разделение парсера и приёма данных от пользователя.

--- main.py
+++ main.py
@@ -9,12 +9,5 @@ if __name__ == "__main__":
     # limit_id, fiat_mas, market_mas, crypto_mas, payment_mas = get_next_position_in_query()
     all_offers = go.get_offers(["RUB"], ["BTC", "ETH", "USDT", "BUSD", "BNB", "SHIB"], [2], ["binance", "bybit", "huobi"],
                         ["Tinkoff", "Sberbank", "Raiffeisenbank"])
-    for offer in all_offers:
-        for el in offer:
-            print("id: ", el.id, " market: ", el.market, " payment: ", el.payment, " sell_buy: ", el.sell_buy,
-                  " init_coin: ", el.init_coin, " receive_coin: ", el.receive_coin, " id_limit: ", el.id_limit,
-                  " price: ", el.price, "maker_commission: ", el.maker_commission,
-                  " taker_commission ", el.taker_commission, " last_time_update: ", el.last_time_update)
-        print("-------------------------------------------------------------------------------------------------------")
-    #print(ans)
-    #print(counter.Counter(ans))
+    for offers in all_offers:
+        print(counter.Counter(offers))

17f03a8735721af35f19a799ca92103b70c1a680
godofcs <chesnov.ilya@bk.ru>
2023-01-05 20:14:44 +0300

    Добавил TODO метку.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.parsers import parse_argument as pa
 from src import counter
 
+# TODO в хелп добавить подсказку про комиссию за перевод.
 
 if __name__ == "__main__":
     #limit_id, fiat_mas, market_mas, crypto_mas, payment_mas = get_next_position_in_query()

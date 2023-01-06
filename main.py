15d285194abc20c42319161821876d0bfd168cd9
godofcs <chesnov.ilya@bk.ru>
2023-01-06 18:46:03 +0300

    Внёс изменения согласно ревью кода.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.parsers import parse_argument as pa
-from src import get_offers as go
+from src import get_offers
 from src import counter
 from src.db_requests import db_session
 
@@ -7,7 +7,7 @@ db_session.global_init("C:/Users/4739409/PycharmProjects/arbitation_project/bd/b
 
 if __name__ == "__main__":
     # limit_id, fiat_mas, market_mas, crypto_mas, payment_mas = get_next_position_in_query()
-    all_offers = go.get_offers(["RUB"], ["BTC", "ETH", "USDT", "BUSD", "BNB", "SHIB"], [2], ["binance", "bybit", "huobi"],
-                        ["Tinkoff", "Sberbank", "Raiffeisenbank"])
+    all_offers = get_offers.get_offers(["RUB"], ["BTC", "ETH", "USDT", "BUSD", "BNB", "SHIB"], [2],
+                                       ["binance", "bybit", "huobi"], ["Tinkoff", "Sberbank", "Raiffeisenbank"])
     for offers in all_offers:
         print(counter.Counter(offers))
--- src/get_offers.py
+++ src/get_offers.py
@@ -5,22 +5,22 @@ from src.db_requests.offers import Offer
 def get_offers(cur_fiat: list, cur_cripto: list, cur_limit_id: list, cur_market: list, cur_payment: list):
     sessions = db_session.create_session()
     all_offers = []
-    for crypto in cur_cripto:
+    for it_crypto in cur_cripto:
         ans = []
-        for payment in cur_payment:
-            for limit_id in cur_limit_id:
-                for market in cur_market:
-                    for fiat in cur_fiat:
-                        offers = sessions.query(Offer).filter(Offer.market == market,
-                                                              Offer.init_coin == fiat,
-                                                              Offer.receive_coin == crypto,
-                                                              Offer.payment == payment,
-                                                              Offer.id_limit == limit_id).all()
-                        rev_offers = sessions.query(Offer).filter(Offer.market == market,
-                                                                  Offer.init_coin == crypto,
-                                                                  Offer.receive_coin == fiat,
-                                                                  Offer.payment == payment,
-                                                                  Offer.id_limit == limit_id).all()
+        for it_payment in cur_payment:
+            for it_limit_id in cur_limit_id:
+                for it_market in cur_market:
+                    for it_fiat in cur_fiat:
+                        offers = sessions.query(Offer).filter(Offer.market == it_market,
+                                                              Offer.init_coin == it_fiat,
+                                                              Offer.receive_coin == it_crypto,
+                                                              Offer.payment == it_payment,
+                                                              Offer.id_limit == it_limit_id).all()
+                        rev_offers = sessions.query(Offer).filter(Offer.market == it_market,
+                                                                  Offer.init_coin == it_crypto,
+                                                                  Offer.receive_coin == it_fiat,
+                                                                  Offer.payment == it_payment,
+                                                                  Offer.id_limit == it_limit_id).all()
                         for offer in offers:
                             ans.append(offer)
                         for offer in rev_offers:
